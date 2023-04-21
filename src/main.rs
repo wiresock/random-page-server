9e5466ddfa1d30c6c607cca9190c58e5effd9f1c
Vadim Smirnov <vadim@ntkernel.com>
2023-04-21 14:58:45 +0200

    Use all CPU cores to enhance the performance.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,74 @@
-use hyper::http::StatusCode;
-/// This code sets up a simple HTTP server that listens on 127.0.0.1:3000.
-/// It responds with "Hello, world!" on the root path ("/") and returns a 404 Not Found status for any other path.
-/// To run the server, simply execute cargo run in your terminal.
+// Importing required libraries for building a web server.
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
+use hyper::http::{StatusCode};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use tokio::sync::oneshot;
+use tokio::runtime::Builder;
 
-#[tokio::main] // Marks the main function as a Tokio runtime to support asynchronous operations.
+// This async function runs the main logic of the program.
+async fn main_async() {
+    // Initializes an address to which the server will listen on.
+    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
 
-async fn main() { // Declares an async main function.
-    let addr = SocketAddr::from(([0, 0, 0, 0], 80)); // Creates a new SocketAddr that can be used to bind to IP and port.
-
-    let make_svc = // Creates a new service using a closure that accepts a connection and returns a future of Result<Response<Body>, Error>.
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
-
-    let server = Server::bind(&addr).serve(make_svc); // Starts a new HTTP server bound with the provided socket address.
-
-    println!("Listening on http://{}", addr); // Prints a message indicating the server is ready.
-    println!("Press ENTER to exit"); // Prints a message indicating how to stop the server.
-
-    let (tx, rx) = oneshot::channel::<()>(); // Sets up a oneshot channel to communicate between threads.
-
-    tokio::spawn(async move { // Spawns a new thread and provides it with an anonymous async block.
-        let mut line = String::new();
-        std::io::stdin().read_line(&mut line).unwrap(); // Reads input from stdin to allow user to terminate the server.
-        let _ = tx.send(()); // Sends a message to the main thread to indicate a termination request.
+    // Defines a `make_service` function that accepts a Connection and returns a new service to handle requests from the connection.
+    let make_svc = make_service_fn(|_conn| {
+        async { Ok::<_, Infallible>(service_fn(handle_request)) }
     });
 
-    let result = tokio::select! { // Waits for either the server to terminate or a termination request to be received.
-        result = server => result,
+    // initializes a server instance that listens on the defined address, and passes the `make_service` function as input.
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Prints a message to indicate the server has started and is listening on a specific port number.
+    println!("Listening on http://{}", addr);
+    // Prompts the user to press ENTER to exit.
+    println!("Press ENTER to exit");
+
+    // Initializes a channel for communication between this function and a spawned thread.
+    let (tx, rx) = oneshot::channel::<()>();
+    // Spawns a new thread using Tokio's runtime, that waits for the user to press ENTER.
+    tokio::spawn(async move {
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let _ = tx.send(());
+    });
+
+    // Waits for either the server to return a result or the thread with `rx` to receive a message
+    let result = tokio::select! {
+        result = server => result, // Server returns Ok() if it completes successfully.
         _ = rx => {
             println!("Shutting down...");
-            Ok(())
+            Ok(()) // Thread returns Ok() if it receives the message successfully.
         }
     };
 
-    if let Err(e) = result { // Prints any errors that occurred during server execution.
+    // Prints any errors that may have occurred during the program's execution.
+    if let Err(e) = result {
         eprintln!("Server error: {}", e);
     }
 }
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> { // Defines an async function that handles incoming HTTP requests.
-    match (req.method(), req.uri().path()) { // Matches the HTTP method and request path.
-        (&hyper::Method::GET, "/") => { // Cases where a GET request is made to the root endpoint.
+// The main function of the program.
+fn main() {
+    // Builds a new Tokio runtime instance for executing async functions.
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(num_cpus::get()) // Sets the number of worker threads to the number of CPUs available.
+        .enable_all()
+        .build()
+        .unwrap();
+    
+    // Executes the `main_async()` function using the previously defined runtime instance.
+    runtime.block_on(main_async());
+}
+
+// Defines an async function that handles incoming HTTP requests.
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // Matches the HTTP method and request path.
+    match (req.method(), req.uri().path()) {
+        // Cases where a GET request is made to the root endpoint.
+        (&hyper::Method::GET, "/") => {
+            // Stores the HTML content of the default Nginx page.
             let nginx_default_page = r#"<!DOCTYPE html>
 <html>
 <head>
@@ -69,15 +93,23 @@ Commercial support is available at
 
 <p><em>Thank you for using nginx.</em></p>
 </body>
-</html>"#; // Stores the HTML content of the default Nginx page.
+</html>"#;
 
-            Ok(Response::new(Body::from(nginx_default_page))) // Returns a response containing the Nginx default page in the body.
+            // Returns a response containing the Nginx default page in the body.
+            Ok(Response::new(Body::from(nginx_default_page)))
         }
-        _ => { // Defaults to handle cases when the requested resource is not available.
-            let mut not_found = Response::default(); // Creates a new response struct with default values.
-            *not_found.status_mut() = StatusCode::NOT_FOUND; // Sets the status code of the response to NOT FOUND.
-            Ok(not_found) // Returns the response.
+        // Defaults to handle cases when the requested resource is not available.
+        _ => {
+            // Creates a new response struct with default values.
+            let mut not_found = Response::default();
+            // Sets the status code of the response to NOT FOUND.
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            // Returns the response.
+            Ok(not_found)
         }
     }
 }
+
+
+
 
