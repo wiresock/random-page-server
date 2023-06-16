ceb3d82eb284fd8bd95a05bb66cc26d79f0b4ba5
Vadim Smirnov <vadim@ntkernel.com>
2023-06-16 11:33:14 +0200

    Append random size text block to default NGINX page.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
 // Importing required libraries for building a web server.
+use hyper::http::StatusCode;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use hyper::http::StatusCode;
+use rand::seq::SliceRandom;
+use rand::Rng;
 use std::convert::Infallible;
 use std::env;
 use std::net::SocketAddr;
-use tokio::sync::oneshot;
+use std::sync::Arc;
 use tokio::runtime::Builder;
+use tokio::sync::oneshot;
 
 const DEFAULT_HTTP_PORT: u16 = 80;
 
+// Function to generate a random alphanumeric string of a certain size
+fn generate_random_alphanumeric(size: usize) -> String {
+    let alphanumeric: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        .chars()
+        .collect();
+    let mut rng = rand::thread_rng();
+    (0..size)
+        .map(|i| {
+            let c = *alphanumeric.choose(&mut rng).unwrap();
+            if i > 0 && i % 60 == 0 {
+                format!("\n{}", c)
+            } else {
+                format!("{}", c)
+            }
+        })
+        .collect()
+}
+
 // This async function runs the main logic of the program.
-async fn main_async(port: u16) {
+async fn main_async(port: u16, random_text: Arc<String>) {
     // Initializes an address to which the server will listen on.
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    
+
     // Defines a `make_service` function that accepts a Connection and returns a new service to handle requests from the connection.
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(handle_request)) }
+    let make_svc = make_service_fn(move |_conn| {
+        let random_text = Arc::clone(&random_text);
+        async {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handle_request(req, Arc::clone(&random_text))
+            }))
+        }
     });
 
     // initializes a server instance that listens on the defined address, and passes the `make_service` function as input.
@@ -62,7 +88,8 @@ fn main() {
     // If exactly one command line argument is present, it will be used as the server's port number.
     if args.len() == 2 {
         // Attempts to parse the command line argument as an unsigned 16 bit integer.
-        port = args[1].parse::<u16>()
+        port = args[1]
+            .parse::<u16>()
             // If the parsing fails, the default value of 80 is used instead.
             .unwrap_or(DEFAULT_HTTP_PORT);
     }
@@ -73,19 +100,26 @@ fn main() {
         .enable_all()
         .build()
         .unwrap();
-    
+
+    // Generate 20KB of random alphanumeric text
+    let random_text = generate_random_alphanumeric(20 * 1024);
+    let random_text = Arc::new(random_text);
+
     // Executes the `main_async()` function using the previously defined runtime instance.
-    runtime.block_on(main_async(port));
+    runtime.block_on(main_async(port, random_text));
 }
 
 // Defines an async function that handles incoming HTTP requests.
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle_request(
+    req: Request<Body>,
+    random_text: Arc<String>,
+) -> Result<Response<Body>, Infallible> {
     // Matches the HTTP method and request path.
     match (req.method(), req.uri().path()) {
         // Cases where a GET request is made to the root endpoint.
         (&hyper::Method::GET, "/") => {
             // Stores the HTML content of the default Nginx page.
-            let nginx_default_page = r#"<!DOCTYPE html>
+            let mut nginx_default_page = r#"<!DOCTYPE html>
 <html>
 <head>
 <title>Welcome to nginx!</title>
@@ -109,9 +143,23 @@ Commercial support is available at
 
 <p><em>Thank you for using nginx.</em></p>
 </body>
-</html>"#;
+</html>"#
+                .to_string();
 
-            // Returns a response containing the Nginx default page in the body.
+            // Generate a random length of random text to be inserted
+            let mut rng = rand::thread_rng();
+            let rand_text_len = rng.gen_range(0..=random_text.len());
+            let random_insert = format!("<p>{}</p>", &random_text[..rand_text_len]);
+
+            // Find the end of the body tag where the random text will be inserted
+            let pos = nginx_default_page
+                .find("</body>")
+                .unwrap_or(nginx_default_page.len());
+
+            // Insert the random text
+            nginx_default_page.insert_str(pos, &random_insert);
+
+            // Returns a response containing the modified Nginx default page in the body.
             Ok(Response::new(Body::from(nginx_default_page)))
         }
         // Defaults to handle cases when the requested resource is not available.
@@ -125,7 +173,3 @@ Commercial support is available at
         }
     }
 }
-
-
-
-
